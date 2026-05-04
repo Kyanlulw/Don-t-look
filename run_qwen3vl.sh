f5a40d4c78a5c839674761e7e4ae894230e4164b
Kyanlulw <datlc81@gmail.com>
2026-05-04 17:52:45 +0700

    kaggle path

--- run_qwen3vl.sh
+++ run_qwen3vl.sh
@@ -7,10 +7,10 @@
 #   ./run_qwen3vl.sh data_a.json data_b.json scored.json . . --batch-size 2
 #   ./run_qwen3vl.sh data_a.jsonl scored.jsonl . --input-2 data_b.jsonl --range-1 0:1000 --range-2 200:1200 --pick-1 300 --pick-2 300 --seed 123
 
-INPUT_JSON="${1:-/root/.cache/kagglehub/datasets/trnlqung/openvivqa/versions/2/openvivqa/non-text-vqa.json}"
+INPUT_JSON="${1:-/kaggle/input/datasets/trnlqung/openvivqa/openvivqa/non-text-vqa.json}"
 # INPUT_JSON_2="${2:-/teamspace/studios/this_studio/.cache/kagglehub/datasets/trnlqung/openvivqa/versions/1/openvivqa_train_v2.json}"
 OUTPUT_JSON="${3:-scored.json}"
-IMAGE_ROOT="${4:-/root/.cache/kagglehub/datasets/trnlqung/openvivqa/versions/2/images/images}"
+IMAGE_ROOT="${4:-/kaggle/input/datasets/trnlqung/openvivqa/images/images}"
 # IMAGE_ROOT_2="${5:-/teamspace/studios/this_studio/.cache/kagglehub/datasets/trnlqung/openvivqa/versions/1/images/images}"
 MODEL="Qwen/Qwen3-VL-8B-Instruct"
 RANGE_1="0:15000"
